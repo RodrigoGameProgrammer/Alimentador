6b72d0f46cc3ce02237fc52b9088a0503630f73c
ShinigamiRodrigo <alesoares.2018@hotmail.com>
2019-08-12 02:13:09 -0300

    Arquivos

---
+++ js/login.js
@@ -0,0 +1,51 @@
+
+firebase.auth().onAuthStateChanged(function(user) {
+  
+  if (user) {
+
+    // User is signed in.
+    document.getElementById("bg-login").style.display = "none";
+    document.getElementById("bg-login2").style.display = "block";
+
+    var user = firebase.auth().currentUser;
+
+	if(user != null){
+
+		var email_id = user.email;
+
+		document.getElementById("user_id").innerHTML = "Bem-Vindo: " + user.email;
+
+	}
+
+  } else {
+
+  	  document.getElementById("bg-login").style.display = "block";
+    document.getElementById("bg-login2").style.display = "none";
+  }
+
+});
+
+function login(){
+
+	var userEmail = document.getElementById("email_field").value;
+	var userPassword = document.getElementById("password_field").value;
+
+	firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+
+	  window.alert("Error: " + errorMessage);
+	  // ...
+	});
+}
+
+function logout(){
+
+	firebase.auth().signOut().then(function() {
+	  // Sign-out successful.
+	}).catch(function(error) {
+	  // An error happened.
+	});
+
+}
