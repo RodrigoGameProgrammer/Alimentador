5cb7605da8e8f5ab785215ff35466ad0ea0dd942
ShinigamiRodrigo <alesoares.2018@hotmail.com>
2019-10-01 01:33:45 -0300

    Atualização

--- js/session.js
+++ js/session.js
@@ -2,6 +2,7 @@
 //----VERIFICAR O ESTADO DA SESSÃO----//
 
 var uid = null;
+var btn_salvar = null;
 
 firebase.auth().onAuthStateChanged(function (user) {
 	if (user) {
@@ -22,29 +23,33 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 //----CONTROLAR O BANCO DE DADOS----//
 
-
-var qtdPorc = null;
-var qtdRacao = null;
-var horario = null;
-var servHorario = null;
-
+//Cria um novo agendamento no banco
 function agendar() {
 
-	qtdPorc = parseInt(document.getElementById("qtdPorc").value);
-	qtdRacao = parseInt(document.getElementById("qtdRacao").value);
-	horario = document.getElementById("horario").value.toString();
-	servHorario = document.getElementById("servHorario").value.toString();
+	//Variaveis 
+	var var_qtdPorc = null;
+	var var_horario = null;
+	var var_servHorario = null;
+	var var_qtdRacao = null;
 
-	console.log(qtdPorc);
-	console.log(qtdRacao);
-	console.log(horario);
-	console.log(servHorario);
+	//Atribuindo valores as variaves e convertendo para inteiro e string
+	var_qtdPorc = parseInt(document.getElementById("qtdPorc").value);
+	var_qtdRacao = parseInt(document.getElementById("qtdRacao").value);
+	var_horario = document.getElementById("horario").value.toString();
+	var_servHorario = document.getElementById("servHorario").value.toString();
 
+	//Mostrar no Log 
+	console.log(var_qtdPorc);
+	console.log(var_qtdRacao);
+	console.log(var_horario);
+	console.log(var_servHorario);
+
+	//Colocando os dados das variveis no banco
 	firebase.database().ref('users/' + uid).set({
-		qtdPorcao: qtdPorc,
-		qtdRacao: qtdRacao,
-		horario: horario,
-		servHorario: servHorario,
+		qtdPorcao: var_qtdPorc,
+		qtdRacao: var_qtdRacao,
+		horario: var_horario,
+		servHorario: var_servHorario,
 	}, function (error) {
 		if (error) {
 			// The write failed...
@@ -54,36 +59,75 @@ function agendar() {
 	});
 }
 
+//Função para ver os agendamentos existentes
 function verAgendamento() {
+
 	firebase.database().ref('users/' + uid).on('value', (snap) => {
-		var qtdPorc2 = snap.child("qtdPorcao").val();
-		var qtdRacao2 = snap.child("qtdRacao").val();
-		var horario2 = snap.child("horario").val();
-		var servHorario2 = snap.child("servHorario").val();
 
+		//Declarando as variaveis e puxando os dados do banco
+		var var_qtdPorc2 = snap.child("qtdPorcao").val();
+		var var_qtdRacao2 = snap.child("qtdRacao").val();
+		var var_horario2 = snap.child("horario").val();
+		var var_servHorario2 = snap.child("servHorario").val();
+
+		//Mostrar no Log 
 		console.log(snap.val());
-		console.log(qtdPorc2 );
-		console.log(qtdRacao2);
-		console.log(horario2);
-		console.log(servHorario2);
+		console.log(var_qtdPorc2);
+		console.log(var_qtdRacao2);
+		console.log(var_horario2);
+		console.log(var_servHorario2);
 
-		document.getElementById("quantidadeP").value = qtdPorc2;
-		document.getElementById("horarioP").value = horario2;
-		document.getElementById("horarioS").value = servHorario2;
-		document.getElementById("quantidadeR").value = qtdRacao2;
-
-		
-		
-		
+		//Trocando os valores dos campos input no HTML
+		document.getElementById("quantidadeP").value = var_qtdPorc2;
+		document.getElementById("horarioP").value = var_horario2;
+		document.getElementById("horarioS").value = var_servHorario2;
+		document.getElementById("quantidadeR").value = var_qtdRacao2;
 	});
 }
 
+//Primeira vez que clicar no botão editar dentro do Modal
+function editar() {
 
-function updateData() {
+	//Liberar os campos para digitar
+	document.getElementById("quantidadeP").disabled = false;
+	document.getElementById("horarioP").disabled = false;
+	document.getElementById("horarioS").disabled = false;
+	document.getElementById("quantidadeR").disabled = false;
+
+	//Botão salvar está ativado
+	btn_salvar = true;
+
+	//Se o botão salvar estivar ativado
+	if (btn_salvar == true) {
+
+		//Troca o texto para Salvar e o onclick para a função atualizar
+		document.getElementById("btn_editar").innerText = "Salvar";
+		document.getElementById("btn_editar").setAttribute('onclick', 'atualizar()');
+
+	} else {
+
+		console.log("error 404, btn_salvar não funciona");
+		return;
+
+	}
+
+}
+
+//Depois que clicar no botão editar ele vira o salvar e ganha a função atualizar
+function atualizar() {
+
+	//Declarando e atribuindo valores as variaves e convertendo para inteiro e string
+	var var_qtdPorc3 = parseInt(document.getElementById("quantidadeP").value);
+	var var_qtdRacao3 = parseInt(document.getElementById("quantidadeR").value);
+	var var_horario3 = document.getElementById("horarioP").value.toString();
+	var var_servHorario3 = document.getElementById("horarioS").value.toString();
+
+	//Função update do Firebase
 	firebase.database().ref('users/' + uid).update({
-		name: "Sasuke2",
-		age: 17,
-		email: "sasuke2@hotmail.com",
+		qtdPorcao: var_qtdPorc3,
+		qtdRacao: var_qtdRacao3,
+		horario: var_horario3,
+		servHorario: var_servHorario3,
 	}, function (error) {
 		if (error) {
 			// The write failed...
@@ -91,8 +135,30 @@ function updateData() {
 			// Data saved successfully!
 		}
 	});
+
+	//Depois do update conferimos se o botão ainda é o salvar 
+	if (btn_salvar == true) {
+
+		//Se for voltamos ele pro editar 
+		btn_salvar = false;
+		document.getElementById("btn_editar").innerText = "Editar";
+		document.getElementById("btn_editar").setAttribute('onclick', 'editar()');
+
+		//Desabilitamos os campos novamente
+		document.getElementById("quantidadeP").disabled = true;
+		document.getElementById("horarioP").disabled = true;
+		document.getElementById("horarioS").disabled = true;
+		document.getElementById("quantidadeR").disabled = true;
+
+	} else {
+
+		console.log("error 404, btn_salvar não funciona");
+		return;
+
+	}
 }
 
+//Deleta o agendamento
 function deletarAgendamento() {
 	firebase.database().ref('users/' + uid).remove();
 }
