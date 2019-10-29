eeec2dd5b88d8f3212a8a2871524f2077271f37b
ShinigamiRodrigo <alesoares.2018@hotmail.com>
2019-10-29 11:11:25 -0300

    Atualização

--- js/login.js
+++ js/login.js
@@ -12,13 +12,12 @@ function login() {
 
 	}).catch(function (error) {
 		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
+		// var errorCode = error.code;
+		// var errorMessage = error.message;
+		// window.alert("Error: " + errorMessage);
+		alert();
+		removeAlert();
 
-		window.alert("Error: " + errorMessage);
-
-
-		// ...
 	});
 }
 
@@ -33,5 +32,31 @@ function logout() {
 	}).catch(function (error) {
 		// An error happened.
 	});
+
+}
+
+
+//----CONTROLAR ALERTAS----//
+
+function alert(){
+	document.getElementById("alertLogin").style.display="block";
+
+}
+
+function removeAlert(){
+
+	email_field.addEventListener('focus', (event) => {
+		document.getElementById("alertLogin").style.display="none";
+	}, true);
+	
+
+	password_field.addEventListener('focus', (event) => {
+		document.getElementById("alertLogin").style.display="none";
+	}, true);
+
+	btnCriarConta.addEventListener('focus', (event) => {
+		document.getElementById("alertLogin").style.display="none";
+		$("#criarConta").modal();
+	}, true);
 
 }
