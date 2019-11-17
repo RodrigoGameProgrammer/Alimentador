1806847613cde3b5c357e1aae9590d0b2b7fe198
ShinigamiRodrigo <alesoares.2018@hotmail.com>
2019-11-17 16:01:30 -0300

    Atualização Design e Galeria

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,34 @@
-function mostraGaleria(){
-    document.getElementById("divGallery").classList.add("gallery");
+function mostrarGaleria(){
+    // document.getElementById("divGallery").classList.remove("fadeIn");
+    document.getElementById("divGallery").classList.add("fadeOut");
+    document.getElementById("divGallery").classList.remove("fadeIn");
+    document.getElementById("divGalleryDisplay").classList.remove("fadeOutLeft");
+    
+    setTimeout(function(){ 
+        document.getElementById("divGallery").style.display = "none";
+        document.getElementById("divGalleryDisplay").style.display = "block";
+        document.getElementById("divGalleryDisplay").classList.add("fadeInLeft");
+    }, 1000);
+  
 }
 
-function mostraMenu(){
-    
+function mostrarMenu(){
+    document.getElementById("divGalleryDisplay").classList.add("fadeOutLeft");
+    document.getElementById("divGalleryDisplay").classList.remove("fadeInLeft");
+    // document.getElementById("divGallery").classList.remove("fadeOut");
+
+    setTimeout(function(){
+        document.getElementById("divGalleryDisplay").style.display = "none";
+        document.getElementById("divGallery").classList.remove("fadeOut");
+        document.getElementById("divGallery").classList.add("fadeIn");
+        document.getElementById("divGallery").style.display = "block";
+    }, 1000);
 }
+
+$(".gallery").magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{
+      enabled: true
+    }
+  });
