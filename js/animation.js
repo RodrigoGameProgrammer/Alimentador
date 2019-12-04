9e758aca7b41fe685fe967861ab59e568b943fbb
ShinigamiRodrigo <alesoares.2018@hotmail.com>
2019-12-03 23:02:35 -0300

    imagens corrigidas navegador

--- js/animation.js
+++ js/animation.js
@@ -32,47 +32,47 @@ var img4 = document.getElementById("img4");
 var img5 = document.getElementById("img5");
 
 function hoverImg1() {
-   img1.src = "../img/icons/dog-icon4.png";
+   img1.src = "./img/icons/dog-icon4.png";
 }
 
 function hoverOutImg1() {
-   img1.src = "../img/icons/dog-icon3.png";
+   img1.src = "./img/icons/dog-icon3.png";
 }
 
 function hoverImg2() {
-   img2.src = "../img/icons/logs-icon3.png";
+   img2.src = "./img/icons/logs-icon3.png";
 }
 
 function hoverOutImg2() {
-   img2.src = "../img/icons/logs-icon2.png";
+   img2.src = "./img/icons/logs-icon2.png";
 }
 
 function hoverImg3() {
-   img3.src = "../img/icons/gallery-icon4.png";
+   img3.src = "./img/icons/gallery-icon4.png";
 }
 
 function hoverOutImg3() {
-   img3.src = "../img/icons/gallery-icon3.png";
+   img3.src = "./img/icons/gallery-icon3.png";
 }
 
 function hoverImg4() {
-   img4.src = "../img/icons/icon_78px/icon-back-orange.png";
+   img4.src = "./img/icons/icon_78px/icon-back-orange.png";
 }
 
 function hoverOutImg4() {
-   img4.src = "../img/icons/icon_78px/icon-back-72.png";
+   img4.src = "./img/icons/icon_78px/icon-back-72.png";
 }
 
 function trocaIcon() {
    setTimeout(function () {
-      img5.src = "../img/icons/logo-icon-white-natal3.png"
+      img5.src = "./img/icons/logo-icon-white-natal3.png"
    }, 1000);
 }
 
 function trocaIcon2() {
 
    setTimeout(function () {
-      img5.src = "../img/icons/logo-icon-white.png"
+      img5.src = "./img/icons/logo-icon-white.png"
    }, 1000);
 
 }
